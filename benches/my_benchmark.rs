ce0538e81fb9e7eea9029e301b4ec08eabadcbee
juliacao <y.cao@berkeley.edu>
2020-10-14 23:15:29 -0700

    blocked multiply and benchmark setup. Need to debug mem leak

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -2,43 +2,43 @@ use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 extern crate matrixmultiply;
 pub use matrixmultiply::dgemm;
 
-fn fibonacci_slow(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci_slow(n - 1) + fibonacci_slow(n - 2),
-    }
-}
-
-fn fibonacci_fast(n: u64) -> u64 {
-    let mut a = 0;
-    let mut b = 1;
-
-    match n {
-        0 => b,
-        _ => {
-            for _ in 0..n {
-                let c = a + b;
-                a = b;
-                b = c;
+// naive 3 loop dgemm
+fn ref_dgemm(lda: usize, a: &Vec<f64>, vec_b: &Vec<f64>, c: &mut Vec<f64>) {
+    for i in 0..lda {
+        for j in 0..lda {
+            let mut cij = c[i + j * lda];
+            for k in 0..lda {
+                cij += a[k + i * lda] * vec_b[k + j * lda];
             }
-            b
+            c[i + j * lda] = cij;
         }
     }
 }
 
-fn bench_fibs(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Fibonacci");
-    for i in [20u64, 21u64].iter() {
-        group.bench_with_input(BenchmarkId::new("Recursive", i), i, |b, i| {
-            b.iter(|| fibonacci_slow(*i))
-        });
-        group.bench_with_input(BenchmarkId::new("Iterative", i), i, |b, i| {
-            b.iter(|| fibonacci_fast(*i))
+fn bench_dgemm(c: &mut Criterion) {
+    let mut group = c.benchmark_group("DGEMM");
+
+    for i in [16, 256].iter() {
+        let mat_a = vec![0.; i * i];
+        let mat_b = vec![0.; i * i];
+        let mut mat_c = vec![0.; i * i];
+        //group.bench_function(BenchmarkId::new("Naive", i), |b| {
+        //    b.iter(|| ref_dgemm(*i as usize, &mat_a, &mat_b, &mut mat_c))
+        //});
+
+        group.bench_function(BenchmarkId::new("Block", i), |b| {
+            b.iter(|| unsafe {
+                dgemm(
+                    *i as usize,
+                    mat_a.as_ptr(),
+                    mat_b.as_ptr(),
+                    mat_c.as_mut_ptr(),
+                )
+            })
         });
     }
     group.finish();
 }
 
-criterion_group!(benches, bench_fibs);
+criterion_group!(benches, bench_dgemm);
 criterion_main!(benches);
--- src/dgemm.rs
+++ src/dgemm.rs
@@ -31,7 +31,7 @@ pub unsafe fn dgemm(lda: usize, a: *const f64, b: *const f64, c: *mut f64) {
                 let k = min(BLOCK_SIZE, lda - k);
                 let a_offset = i + k * lda;
                 let b_offset = k + j * lda;
-                copy_col_major(
+                copy_row_major(
                     a.offset(a_offset as isize),
                     BLOCK_SIZE,
                     k,
@@ -47,12 +47,95 @@ pub unsafe fn dgemm(lda: usize, a: *const f64, b: *const f64, c: *mut f64) {
                     lda,
                     block_b.as_mut_ptr(),
                 );
+                do_block(
+                    BLOCK_SIZE,
+                    block_a.as_mut_ptr(),
+                    block_b.as_mut_ptr(),
+                    block_c.as_mut_ptr(),
+                );
+                let offset = i + j * lda;
+                unpad_col_to_col_dst(
+                    block_c.as_ptr(),
+                    BLOCK_SIZE,
+                    n,
+                    m,
+                    c.offset(offset as isize),
+                    lda,
+                );
             }
         }
     }
 }
 
-unsafe fn copy_col_major(
+unsafe fn unpad_col_to_col_dst(
+    x: *const f64,
+    n: usize,
+    right: usize,
+    down: usize,
+    dst: *mut f64,
+    col: usize,
+) {
+    for i in 0..n {
+        for j in 0..n {
+            if !(i >= down || j >= right) {
+                let dst_offset = j * col + i;
+                let x_offset = i + n * j;
+                ptr::write(
+                    dst.offset(dst_offset as isize),
+                    ptr::read(x.offset(x_offset as isize)),
+                );
+            }
+        }
+    }
+}
+
+unsafe fn copy_row_major(
+    x: *const f64,
+    n: usize,
+    right: usize,
+    down: usize,
+    l: usize,
+    temp: *mut f64,
+) {
+    // for each col of x
+    for i in 0..n {
+        // for each row of x
+        for j in 0..n {
+            let offset = j * n + i;
+            if j >= down {
+                ptr::write(temp.offset(offset as isize), 0f64);
+            } else if i >= right {
+                ptr::write(temp.offset(offset as isize), 0f64);
+            } else {
+                let x_offset = i * l + j;
+                ptr::write(
+                    temp.offset(offset as isize),
+                    ptr::read(x.offset(x_offset as isize)),
+                );
+            }
+        }
+    }
+}
+
+unsafe fn do_block(lda: usize, a: *mut f64, b: *mut f64, c: *mut f64) {
+    // for each row i of A
+    for i in 0..lda {
+        // for each col j of b
+        for j in 0..lda {
+            let offset = i + j * lda;
+            let mut cij = ptr::read(c.offset(offset as isize));
+            for k in 0..lda {
+                let a_offset = k + i * lda;
+                let b_offset = k + j * lda;
+                cij +=
+                    ptr::read(a.offset(a_offset as isize)) * ptr::read(b.offset(b_offset as isize));
+            }
+            ptr::write(c.offset(offset as isize), cij);
+        }
+    }
+}
+
+/*unsafe fn copy_col_major(
     x: *const f64,
     n: usize,
     right: usize,
@@ -82,6 +165,32 @@ unsafe fn copy_col_major(
         for i in 0..n {
             for j in (0..n).step_by(16) {
                 //use avx
+            }
+        }
+    }
+}*/
+
+unsafe fn copy_col_major(
+    x: *const f64,
+    n: usize,
+    right: usize,
+    down: usize,
+    l: usize,
+    temp: *mut f64,
+) {
+    for i in 0..n {
+        for j in 0..n {
+            let offset = i * n + j;
+            if j >= down {
+                ptr::write(temp.offset(offset as isize), 0f64);
+            } else if i >= right {
+                ptr::write(temp.offset(offset as isize), 0f64);
+            } else {
+                let x_offset = i * l + j;
+                ptr::write(
+                    temp.offset(offset as isize),
+                    ptr::read(x.offset(x_offset as isize)),
+                );
             }
         }
     }
