5649d986c82c82e864ee6c8a1590f77b49fa3f1c
juliacao <y.cao@berkeley.edu>
2020-10-15 10:25:37 -0700

    updated readme and benchmark report for block vs naive

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-## dgemm implementation in Rust
-* using criterion for benchmark
+# DGEMM in Rust
+
+Input matrices are column major
+
+### System
+OS: MacOS Catalina 10.15.6
+
+Processor: 2.3 GHz Quad-Core Intel Core i7
+
+Memory: 16 GB 3733 MHz LPDDR4X
+
+## Strategy
+
+### Block Multiply
+* Breaks matrices into blocks that fit into l1 cache
+* Copies matrices into column major and row major respectively for optimal access pattern
+* Performs normal 3 loop once we are inside a small block
+* Optimizes for block size 4096
+* Achieve ~4x performance improvement
+* [Performance report](./benchmark_report/block_vs_naive.pdf)
+
+## Tools
+* criterion for benchmark
+
+
--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -1,6 +1,7 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 extern crate matrixmultiply;
 pub use matrixmultiply::dgemm;
+use rand::Rng;
 
 // naive 3 loop dgemm
 fn ref_dgemm(lda: usize, a: &Vec<f64>, vec_b: &Vec<f64>, c: &mut Vec<f64>) {
@@ -17,11 +18,12 @@ fn ref_dgemm(lda: usize, a: &Vec<f64>, vec_b: &Vec<f64>, c: &mut Vec<f64>) {
 
 fn bench_dgemm(c: &mut Criterion) {
     let mut group = c.benchmark_group("DGEMM");
+    let mut rng = rand::thread_rng();
 
-    for i in [16, 256].iter() {
-        let mat_a = vec![0.; i * i];
-        let mat_b = vec![0.; i * i];
-        let mut mat_c = vec![0.; i * i];
+    for i in [31, 32, 96, 97, 127, 128, 129, 229, 255, 256, 257, 319, 320, 321, 512, 767, 768, 769].iter() {
+        let mat_a = vec![rng.gen::<f64>(); i * i];
+        let mat_b = vec![rng.gen::<f64>(); i * i];
+        let mut mat_c = vec![rng.gen::<f64>(); i * i];
         group.bench_function(BenchmarkId::new("Naive", i), |b| {
            b.iter(|| ref_dgemm(*i as usize, &mat_a, &mat_b, &mut mat_c))
         });
